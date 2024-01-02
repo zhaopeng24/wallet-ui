5addf13f03a8864b0b05c4d3949223ede3eb0282
gray_jiang <Gray_Jy@yeah.net>
2024-01-03 01:40:05 +0800

    🐳 chore(wagmi config)

---
+++ constants/constants.ts
@@ -0,0 +1,5 @@
+const AVALANCHE_HTTP_RPC = 'https://api.avax.network/ext/bc/C/rpc'
+const AVALANCHEFUJI_HTTP_RPC = 'https://api.avax-test.network/ext/C/rpc'
+const MOONBASE_HTTP_RPC = 'https://moonbase.unitedbloc.com'
+
+export { AVALANCHE_HTTP_RPC, AVALANCHEFUJI_HTTP_RPC, MOONBASE_HTTP_RPC }
