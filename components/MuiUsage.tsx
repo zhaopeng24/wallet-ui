ab81c0f75293aa99edb765244dfef0e5cb8728ac
gray_jiang <Gray_Jy@yeah.net>
2024-01-03 00:32:43 +0800

    🎉 init

---
+++ components/MuiUsage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+
+const MuiUsage = () => {
+  return (
+    <Button variant='contained' className='text-red-700'>
+      Hello world
+    </Button>
+  )
+}
+
+export default MuiUsage
