15775656e08d0e7d7517a1922e200aefe9e1237a
fnkk <1748501327@qq.com>
2024-02-02 15:41:44 +0800

    feat: :zap: Complete direct transfer related tasks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        customBlue: '#23377C',
+        royalBlue:'#425EB1'
+      },
     },
   },
   darkMode: "class",
