d151e8c06407349cf6f83c55860c2c4b6f275c36
hukai <hukai@cvte.com>
2024-02-23 16:22:42 +0800

    feat: polish demand page

--- src/api/hold.ts
+++ src/api/hold.ts
@@ -1,50 +1,52 @@
-import { Response, AssetBalance } from "./types/hold"
+import { Response, AssetBalance } from "./types/hold";
 
-export async function getAssetBalance(chainId: number, address: string): Promise<Response<AssetBalance>> {
+export async function getAssetBalance(
+  chainId: number,
+  address: string
+): Promise<Response<AssetBalance>> {
   try {
-    // const res = await fetch("", { cache: 'no-store' })
     const dataR = {
       chainId: { chainId },
-      address: { address }
-    }
+      address: { address },
+    };
     const res = await fetch("https://asset-dev.web3idea.xyz/api/v1/balance", {
       method: "POST",
       cache: "no-cache",
-      body: JSON.stringify(dataR)
-    })
-    const data: Response<AssetBalance> = await res.json()
-    return data
+      body: JSON.stringify(dataR),
+    });
+    const data: Response<AssetBalance> = await res.json();
+    return data;
   } catch (error) {
-    return Promise.reject(error)
+    return Promise.reject(error);
   }
 }
 
 export async function getV1Config() {
   try {
     const res = await fetch("https://asset-dev.web3idea.xyz/api/v1/package");
-    const data = await res.json()
-    return data
+    const data = await res.json();
+    return data;
   } catch (error) {
-    console.log('get config error')
+    console.log("get config error");
   }
 }
 
 export async function getTxDetail(chainId: number, address: string) {
   try {
     const dataR = {
-      "chainId": 1,
-      "address": "0x61f8a7B1634F3AfD82c13F01b995187432E85eEf",
-    }
+      chainId: 1,
+      address: "0x61f8a7B1634F3AfD82c13F01b995187432E85eEf",
+    };
     const res = await fetch("https://asset-dev.web3idea.xyz/api/v1/tx/list", {
       method: "POST",
       cache: "no-cache",
       body: JSON.stringify(dataR),
-      redirect: 'follow'
-    })
-    const data = await res.json()
-    return data
+      redirect: "follow",
+    });
+    const data = await res.json();
+    return data;
   } catch (error) {
-    console.log(`this is get tx detail error ${error}`)
+    console.log(`this is get tx detail error ${error}`);
   }
 }
 export async function getTxList(chainId: number, address: string) {
@@ -56,18 +58,17 @@ export async function getTxList(chainId: number, address: string) {
   myHeaders.append("Connection", "keep-alive");
 
   var raw = JSON.stringify({
-    "chainId": chainId,
-    "address": "0x61f8a7B1634F3AfD82c13F01b995187432E85eEf"
+    chainId: chainId,
+    address: "0x61f8a7B1634F3AfD82c13F01b995187432E85eEf",
   });
 
-
   const res = await fetch("https://asset-dev.web3idea.xyz/api/v1/tx/list", {
-    method: 'POST',
+    method: "POST",
     headers: myHeaders,
     body: raw,
-    redirect: 'follow'
-  })
+    redirect: "follow",
+  });
 
-  const data = await res.json()
-  return data
+  const data = await res.json();
+  return data;
 }
