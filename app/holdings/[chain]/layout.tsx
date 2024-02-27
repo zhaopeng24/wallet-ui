1078fed05b0176d3a734b7dc61feea3b40fc725f
hukai <hukai@cvte.com>
2024-02-27 22:31:14 +0800

    feat: remove code

--- app/holdings/[chain]/layout.tsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from "@/components/basic/MainLayout";
-
-export default function ChainLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <MainLayout>
-      {children}
-    </MainLayout>
-  )
-}
