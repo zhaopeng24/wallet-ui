584c66b458c7dfbf80bca56ee14b78e083219fa3
hukai <hukai@cvte.com>
2024-03-28 11:11:41 +0800

    feat: test

--- src/app/transfer_function/choosetoken/page.tsx
+++ src/app/transfer_function/choosetoken/page.tsx
@@ -1,38 +1,35 @@
 "use client";
+
 import { Avatar } from "@nextui-org/react";
 import Token from "../components/Token";
-import { IChain, ITokenBalance, useChains } from "@/store/useChains";
+import { IChain, useChains } from "@/store/useChains";
 import { useMemo } from "react";
+import { formatAddress } from "@/utils/format";
 
 export default function ChooseToken() {
   const { chains, balances } = useChains((state) => state);
-  const transferData = useMemo(() => {
-    const address = sessionStorage.getItem("transfer_address") || "";
-    const name = sessionStorage.getItem("transfer_name") || "";
-    const chainId = sessionStorage.getItem("transfer_chainId");
-    const findChain = chains.find((c: IChain) => c.ID === Number(chainId));
-    const balance = balances.find((b) => b.chainName === findChain?.name);
-    const _banlance = [];
-    if (balance) {
-      if (balance.NativeBalance) {
-        _banlance.push(balance.NativeBalance);
-      }
-      if (balance.tokenBalance) {
-        _banlance.push(...balance.tokenBalance);
-      }
-    }
-    return {
-      address,
-      name,
-      chain: findChain,
-      tokens: findChain?.tokens,
-      balances: _banlance,
-    };
-  }, []);
-
-  function formattedAddr(address: string | null) {
-    return address?.substring(0, 7) + "..." + address?.substring(38);
-  }
+  // const transferData = useMemo(() => {
+  //   const address = sessionStorage.getItem("transfer_address") || "";
+  //   const name = sessionStorage.getItem("transfer_name") || "";
+  //   const chainId = sessionStorage.getItem("transfer_chainId");
+  //   const findChain = chains.find((c: IChain) => c.ID === Number(chainId));
+  //   const balance = balances.find((b) => b.chainName === findChain?.name);
+  //   const _banlance = [];
+  //   if (balance) {
+  //     if (balance.NativeBalance) {
+  //       _banlance.push(balance.NativeBalance);
+  //     }
+  //     if (balance.tokenBalance) {
+  //       _banlance.push(...balance.tokenBalance);
+  //     }
+  //   }
+  //   return {
+  //     address,
+  //     name,
+  //     tokens: findChain?.tokens,
+  //     balances: _banlance,
+  //   };
+  // }, []);
 
   return (
     <div className="p-6">
@@ -40,13 +37,13 @@ export default function ChooseToken() {
       <div className="flex justify-bettwen items-center">
         <div className="flex items-center ">
           <Avatar className="mr-4"></Avatar>
-          <div className="flex flex-col">
+          {/* <div className="flex flex-col">
             <p className="">{transferData.name || "NoName"}</p>
-            <p className="">{formattedAddr(transferData.address)}</p>
-          </div>
+            <p className="">{formatAddress(transferData.address)}</p>
+          </div> */}
         </div>
       </div>
-      <Token tokens={transferData.tokens} balances={transferData.balances} />
+      {/* <Token tokens={transferData.tokens} balances={transferData.balances} /> */}
     </div>
   );
 }
