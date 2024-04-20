07ba98e54092bce2364e7e220acbd4bf8f5a2847
szt <1075097965@qq.com>
2024-04-20 10:28:48 +0800

    提交信息

--- components/MuiUsage.tsx
+++ components/MuiUsage.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import Button from '@mui/material/Button'
+import React from 'react';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const MuiUsage = () => {
+  const handleGoBack = () => {
+    window.history.back();
+  };
+
   return (
+    <div>
+      <div style= {{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center',marginBottom: 16 }}>
+        <IconButton onClick= {handleGoBack}>
+        <ArrowBackIcon />
+        </IconButton>
+        </div>
+      <p>Sign Up </p>
     <Button variant='contained' className='text-red-700'>
       Hello world
     </Button>
+    </div>
   )
 }
 
