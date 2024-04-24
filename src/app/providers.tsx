c266f54be32c7df9c60290830d88ee8ad8f2dd27
capi <capiiii@outlook.com>
2024-04-24 14:40:22 +0800

    fix bug 59/68

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,8 +6,10 @@ import { Global } from "@/server/Global";
 import { Config } from "@/server/config/Config";
 import { useChains } from "@/store/useChains";
 import { NextUIProvider } from "@nextui-org/react";
-import { useRouter } from "next/navigation";
 import { createContext, useCallback, useEffect, useRef, useState } from "react";
+import { JSONBigInt } from "@/server/js/common_utils";
+import { useAddress } from "@/store/useAddress";
+import { CalcWalletAddress } from "@/api/auth";
 
 interface ILoadingContextProps {
   loading: boolean;
@@ -15,16 +17,19 @@ interface ILoadingContextProps {
 }
 export const LoadingContext = createContext<ILoadingContextProps>({
   loading: false,
-  setLoading: () => {},
+  setLoading: () => { },
 });
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [loading, setLoading] = useState(false);
   const [text, setText] = useState("loading...");
   const [inited, setInited] = useState(false);
-  const router = useRouter();
 
-  const { setChains } = useChains((state) => state);
+  const { setMpcAddress, setAddressList, setCurrentAddress, } = useAddress(
+    (state) => state
+  );
+
+  const { setChains, setCurrentChain } = useChains((state) => state);
 
   // todo 把loading状态放到zustand里面
   const setLoadingFn = useCallback((loading: boolean, text?: string) => {
@@ -51,12 +56,25 @@ export function Providers({ children }: { children: React.ReactNode }) {
       await Global.init({
         mpcWasmUrl: Config.MPC_WASM_URL,
         rpcUrl: "",
-        authorization: "",
+        authorization: localStorage.getItem("authorization") || "",
         backendApiUrl: Config.BACKEND_API,
         storageApiUrl: Config.DECENTRALIZE_STORAGE_API,
       });
 
       setChains(chain);
+
+      if (localStorage.getItem("authorization")) {
+        Global.account.setAuthorization(Global.authorization);
+        Global.account.setBlockchainRpc(chain[0].rpcApi!);
+        await Global.account.initAccount(JSONBigInt.stringify(localStorage.getItem("mpc_key_local")));
+        const mpcAddress = await Global.account.getOwnerAddress();
+        setMpcAddress(mpcAddress);
+        const res = await CalcWalletAddress(mpcAddress);
+        const data = res.body.result;
+        setAddressList(data);
+        setCurrentAddress(chain[0].ID!);
+      }
+
       localStorage.setItem("wallet_chains", JSON.stringify(chain));
       setLoading(false);
       setInited(true);
--- src/app/transfer_function/page.tsx
+++ src/app/transfer_function/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Sent from "./components/Sent";
-import Contact from "./components/Contact";
+import Contact, { ConfirmFunc, ContactData } from "./components/Contact";
 import { Button } from "@nextui-org/react";
 import { useState } from "react";
 import { IChain } from "@/store/useChains";
 import { useRouter } from "next/navigation";
+import Toast from "@/utils/toast";
 
 export default function Transfer_Function() {
   const [address, setAddress] = useState("");
@@ -12,21 +13,42 @@ export default function Transfer_Function() {
   const router = useRouter();
 
   function handleNext() {
+    if (!/^(0x)?[0-9a-fA-F]{40}$/.test(address)) {
+      Toast("The address format is incorrect")
+      return
+    }
+
     if (address && chain) {
+      const _cl = localStorage.getItem("contact_list")
+      const contactList = _cl ? JSON.parse(_cl) : [];
+      const contact = contactList.find(
+        (item: ContactData) => item.address == address && item.chain.id == chain.ID.toString()
+      )
+      sessionStorage.setItem("transfer_name", contact?.name || "")
       sessionStorage.setItem("transfer_address", address);
-      sessionStorage.setItem("transfer_chainId", chain.ID + "");
+      sessionStorage.setItem("transfer_chainId", chain.ID.toString());
       router.push("/chooseToken");
     }
   }
+  const confirmContact: ConfirmFunc = ({
+    addr,
+    chain_id,
+    name
+  }) => {
+    sessionStorage.setItem("transfer_address", addr);
+    sessionStorage.setItem("transfer_chainId", chain_id.toString());
+    sessionStorage.setItem("transfer_name", name);
+    router.push("/chooseToken");
+  }
   return (
-    <div className="p-6">
+    <div className="w-full">
       <Sent
         address={address}
         chain={chain}
         setAddress={setAddress}
         setChain={setChain}
       />
-      {/* <Contact /> */}
+      <Contact confirmContact={confirmContact} />
       <div className="absolute bottom-8 left-8 right-8">
         <Button
           onClick={handleNext}
