698545300fb067658a307042afef89ca0122403c
TerrySu <bjsuzhe@163.com>
2024-04-26 10:33:57 +0800

    feature chooseToken cross-chain SWT not show amount

--- src/app/chooseToken/page.tsx
+++ src/app/chooseToken/page.tsx
@@ -3,18 +3,30 @@ import Header from "@/components/Header";
 import MainLayout from "@/components/basic/MainLayout";
 import { Avatar } from "@nextui-org/react";
 import Token from "../transfer_function/components/Token";
-import { IChain, useChains } from "@/store/useChains";
+import { IBalance, IChain, ITokenBalance, useChains } from "@/store/useChains";
 import { useMemo } from "react";
 import { formatAddress } from "@/utils/format";
 
+const crossChainSwt = (banlance: ITokenBalance[], balance: IBalance | undefined) => {
+  if (!balance) return;
+  const _banlance = [];
+  if (balance.NativeBalance) _banlance.push(balance.NativeBalance);
+  if (balance.tokenBalance) _banlance.push(...balance.tokenBalance);
+  const currentSwt = _banlance.find(({tokenId}) => tokenId === 2);
+  if (!currentSwt) return;
+  const index = banlance.findIndex((b) => b.tokenId === 2);
+  if (index!== -1) banlance.splice(index, 1, currentSwt);
+}
+
 export default function ChooseToken() {
-  const { chains, balances } = useChains((state) => state);
+  const { chains, balances, currentChain } = useChains((state) => state);
   const transferData = useMemo(() => {
     const address = sessionStorage.getItem("transfer_address") || "";
     const name = sessionStorage.getItem("transfer_name") || "";
     const chainId = sessionStorage.getItem("transfer_chainId");
     const findChain = chains.find((c: IChain) => c.ID === Number(chainId));
     const balance = balances.find((b) => b.chainName === findChain?.name);
+    const isCrossChain = currentChain?.ID !== Number(chainId)
     const _banlance = [];
     if (balance) {
       if (balance.NativeBalance) {
@@ -24,6 +36,9 @@ export default function ChooseToken() {
         _banlance.push(...balance.tokenBalance);
       }
     }
+    // crossChain
+    if (isCrossChain) crossChainSwt(_banlance, balances.find((b) => b.chainName === currentChain?.name))
+
     return {
       address,
       name,
