9c74cb4a1bce663d0bb3709c6782d87232793e2d
Bronx <shenzetong28@gmail.com>
2024-05-06 14:39:24 +0800

    更改

--- app/pages/page.tsx
+++ app/pages/page.tsx
@@ -1,11 +1,77 @@
-import MuiUsage from '@/components/MuiUsage'
+"use client";
+import { Button } from "@nextui-org/button";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
-  return (
-    <main className='min-h-screen'>
-      <div>
-        <MuiUsage />
-      </div>
-    </main>
-  )
+const Home = () => {
+  const router = useRouter();
+  function handleLogin() {
+    router.replace("/login");
+  }
+  function handleRegister() {
+    router.replace("/register");
+  }
+  function isRegistered() {
+  // 如果本地保存的key不为空，则表示已注册过账号
+  // 否则表示未注册过账号
+  const result = localStorage.getItem('mpc_key_local');
+  return Boolean(result);
 }
+  return (
+    <div className="container h-full bg-[url(/imgs/home-bg.png)] bg-cover">
+      <div className="main p-8 mb-32">
+        <img className="w-40 mb-16" src="/imgs/home-logo.png" alt="" />
+        <img className="w-44 mb-6" src="/imgs/home-text1.png" alt="" />
+        <img className="w-56" src="/imgs/home-text2.png" alt="" />
+      </div>
+      <div className="flex px-8 opacity-80">
+        <div className="flex-1 text-center text-xs">
+          <img
+            className="inline-block w-9 h-9"
+            src="/imgs/home-demand.png"
+            alt=""
+          />
+          <div>Demand</div>
+          <div>Abstraction</div>
+        </div>
+        <div className="flex-1 text-center text-xs">
+          <img
+            className="inline-block w-9 h-9"
+            src="/imgs/home-demand.png"
+            alt=""
+          />
+          <div>Account</div>
+          <div>Abstraction</div>
+        </div>
+        <div className="flex-1 text-center text-xs">
+          <img
+            className="inline-block w-9 h-9"
+            src="/imgs/home-demand.png"
+            alt=""
+          />
+          <div>Automated</div>
+          <div>AI trading bot</div>
+        </div>
+      </div>
+      <div className="p-8 text-xs">
+        {isRegistered()?
+        <Button
+          fullWidth
+          size="lg"
+          onClick={handleLogin}
+          className="text-xs bg-white rounded-3xl text-black font-semibold mb-5 "
+        >
+          Login
+        </Button>:null}
+        <Button
+          fullWidth
+          size="lg"
+          onClick={handleRegister}
+          className="text-xs bg-[#819DF5] rounded-3xl font-semibold"
+        >
+          Register
+        </Button>
+      </div>
+    </div>
+  );
+};
+export default Home;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,44 @@
-<<<<<<< HEAD
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-=======
 import type { Config } from "tailwindcss";
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
->>>>>>> 46e3dff45f1e10a18c8e69e466c68ba930f7a0db
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "500px",
+      },
+    },
     extend: {
       backgroundImage: {
-<<<<<<< HEAD
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-=======
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
->>>>>>> 46e3dff45f1e10a18c8e69e466c68ba930f7a0db
       },
+      colors: {
+        customBlue: "#23377C",
+        royalBlue: "#425EB1",
+        purpleBlue:"#819DF580",
+        mainPurpleBlue:"#819DF5"
+      },
+      // screens: {
+      //   'md-max': {
+      //     raw: '(max-width: 768px)',
+      //   },
+      //   'lg-max': {
+      //     raw: '(max-width: 1024px)',
+      //   },
+      // },
     },
   },
-  plugins: [],
-<<<<<<< HEAD
-}
-export default config
-=======
+  darkMode: "class",
+  plugins: [nextui()],
 };
 export default config;
->>>>>>> 46e3dff45f1e10a18c8e69e466c68ba930f7a0db
